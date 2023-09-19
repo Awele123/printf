8620087a835a898d573fe5cb462b74988bc10fe7
awele123 <nwabuzorawele42@gmail.com>
2023-09-19 01:35:20 -0700

    mainfile

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int print_c(char c);
+int print_percent(void);
+
+#endif
